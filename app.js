cdf7fe202e27b84f484dba9c13d6a52395c498b4
sanjaymsk12-ai <sanjaymsk12@gmail.com>
2025-12-15 08:48:29 +0530

    Update scss

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 let nav = document.getElementById("nav");
 
 
-
-
 window.addEventListener("wheel", function(event){
     if(event.deltaY > 0){
         // Scroll Down
         nav.style.position = "fixed";
         nav.style.top = "-100px";
-        nav.style.transition = "top 2s";
+        nav.style.transition = "top 0.5s";
         console.log("scroll down");
-        
     }
     else{
         // Scroll Up
         nav.style.position = "fixed";
         nav.style.top = "0px";
-        nav.style.transition = "top 2s";
+        nav.style.transition = "top 0.5s";
         console.log("scroll up");
     }
 });
