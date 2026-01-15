6b1f7046016bf14fcd35f1f32ec7d93cfdf6ed08
sanjaymsk12-ai <sanjaymsk12@gmail.com>
2026-01-15 21:10:43 +0530

    pomodoro page display none

--- Productivity Manager/Productity_Monitor.js
+++ Productivity Manager/Productity_Monitor.js
@@ -163,39 +163,172 @@ function SaveInputsToLocalstorage(){
 
 SaveInputsToLocalstorage();
 
-async function GetQuote(){
+///-----------------------------Motivational quote page------------------------
+
+async function GetQuote_Date_time(){
     let MotivationQuote = document.querySelector('.quote-card p')
     let MotivationAuthor = document.querySelector('.quote-card .author h2')
-    
-    let response =await fetch('http://api.quotable.io/random');
-    let data = await response.json()
-    
-    // console.log(data.content);
-    // console.log(data.author);
-
-    MotivationQuote.innerHTML = data.content
-    MotivationAuthor.innerHTML = data.author
-}
-
-GetQuote();
-
-function GetDate_time(){
     let Day_TimeBox =document.querySelector('.day-time')
+    let speak =document.querySelector('.motivation-fullpage .speak')
 
+    
     let d = new Date();
     let day = d.toLocaleDateString("en-US", { weekday: "short" });
     let month = d.toLocaleDateString("en-US", { month: "long" });
     let year = d.getFullYear();
-
-    console.log(day , month , year);
+    
+    
     Day_TimeBox.innerHTML= `<div class="box today">
-                                    <h1>${d.getDate()} <span class="day">${day}</span><br> <span class="month" >${month}</span></h1>
-                                    <h5>${year}</h5>
-                            </div>
-                             <div class="box day-lime">
-                                    <h1>Quote of the Day.</h1>
-                            </div>`
+    <h1>${d.getDate()} <span class="day">${day}</span><br> <span class="month" >${month}</span></h1>
+    <h5>${year}</h5>
+    </div>
+    <div class="box day-lime">
+    <h1>Quote of the Day.</h1>
+    </div>`
+    
+    let response =await fetch('https://motivational-spark-api.vercel.app/api/quotes/random/10');
+    let data =  await response.json()
+    
+    // console.log(data[0].quote);
+    
+    MotivationQuote.innerHTML = data[0].quote
+    MotivationAuthor.innerHTML = data[0].author
+
+
+    speak.addEventListener('click',()=>{
+        let Date_voice = new SpeechSynthesisUtterance( `${d.getDate()} ${month}  ${year}`)
+        speechSynthesis.speak(Date_voice);
+
+        let msg = new SpeechSynthesisUtterance(`Today's quote of the day :- ${data[0].author} said, "${data[0].quote}"`);
+        msg.lang = "hi-IN";
+        speechSynthesis.speak(msg);
+        console.log(`${d.getDate()} ${month}  ${year}`);
+    })
     
 }
 
-GetDate_time();
+GetQuote_Date_time();
+
+//-----------------------------Pomodora page----------------------------
+
+
+let Work_inputTime = 5;
+let Break_inputTime = 2;
+
+function GetUserinput(){
+    let Setting = document.querySelector('.pomodoro-fullpage img');
+    let inputBox = document.querySelector('.pomodoro-fullpage .user-input');
+    let inputs = document.querySelectorAll('.pomodoro-fullpage input');
+
+    let isVisible = false;
+    // inputBox.style.left = '-100%';
+
+    Setting.addEventListener('click',function(){
+        if(isVisible == false){
+            inputBox.style.left = '2.2rem';
+            isVisible = true;
+        }else{
+            inputBox.style.left = '-100%';
+            isVisible = false;
+        }
+    
+    })
+
+    
+    inputs[0].addEventListener('input',function(){
+        Work_inputTime = inputs[0].value
+        totalsecond = Work_inputTime*60
+        clearInterval(interval);
+        console.log(Work_inputTime,Break_inputTime , totalsecond);
+        AddTimer()
+    })
+    inputs[1].addEventListener('input',function(){
+        Break_inputTime = inputs[1].value
+        clearInterval(interval);
+        console.log(Work_inputTime,Break_inputTime);
+        AddTimer()
+    })
+
+    
+
+}
+
+GetUserinput();
+
+let Title = document.querySelector('.pomodoro-main .top h1')
+let Timebox = document.querySelector('.pomodoro-main .middle .time')
+
+let isWorksession = true;
+let totalsecond = Work_inputTime*60;
+var interval=null
+
+function AddTimer(){
+    let minutes = Math.floor(totalsecond/60)
+    let seconds = totalsecond%60
+    console.log(minutes,seconds);
+    
+    Timebox.innerHTML = `${String(minutes).padStart("2","0")}:${String(seconds).padStart("2","0")}`
+}
+
+AddTimer();
+
+
+function DTimer(){
+        clearInterval(interval);
+        if (isWorksession) {
+            interval = setInterval(() => {
+                if (totalsecond > 0) {
+                    totalsecond--;
+                    AddTimer();
+                } else {
+                    isWorksession = false;
+                    Title.innerHTML = "Break";
+                    Title.style.backgroundColor = "var(--blue)";
+                    totalsecond = Break_inputTime*60;
+                    AddTimer();
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }else{
+            interval = setInterval(() => {
+                if (totalsecond > 0) {
+                    totalsecond--;
+                    AddTimer();
+                } else {
+                    isWorksession = true;
+                    Title.innerHTML = "Work Session";
+                    Title.style.backgroundColor = "var(--green)";
+                    totalsecond = Work_inputTime*60;
+                    AddTimer();
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+
+    }
+
+// DTimer();
+
+function ButtonsClickEvent(){
+    let startBtn = document.querySelector('.pomodoro-main .bottom #start')
+    let pauseBtn = document.querySelector('.pomodoro-main .bottom #pause')
+    let resetBtn = document.querySelector('.pomodoro-main .bottom #reset')
+    
+    startBtn.addEventListener('click',()=>{
+            DTimer();
+    })
+    
+    pauseBtn.addEventListener('click',()=>{
+            clearInterval(interval)
+    })
+    
+    resetBtn.addEventListener('click',()=>{
+            totalsecond=21
+            AddTimer();
+    })
+}
+ButtonsClickEvent();
+
+
+
+
